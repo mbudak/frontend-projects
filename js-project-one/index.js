ee9fc85ab9ccb2da779f445a503d82d25bc702a8
mbudak <promaxcoq@gmail.com>
2022-05-10 09:15:51 -0400

    first js project init

---
+++ js-project-one/index.js
@@ -0,0 +1,12 @@
+var age, year;
+
+age = 20;
+year = 2000;
+
+
+
+// alert('check your console log');
+console.log(age, year);
+document.write("age is ", age);
+
+document.write(" and the year is ", year);
