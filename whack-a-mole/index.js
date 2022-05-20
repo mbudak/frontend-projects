9000c200caf2e51895c63853a219a3e2f7968ab1
mbudak <promaxcoq@gmail.com>
2022-05-19 20:22:55 -0400

    whack a mole done

---
+++ whack-a-mole/index.js
@@ -0,0 +1,25 @@
+let score = 0;
+
+const holes = document.getElementsByClassName('hole');
+
+const gameArea = document.getElementById('hit-a-mole');
+
+gameArea.addEventListener('click', function (clickEvent) {
+    // if we hit then add it to score
+    if (clickEvent.target.matches('.mole')) {
+    score++;
+    document.getElementById('score').innerText = score;
+  }
+});
+
+
+// Game play interval each 1.5 seconds
+setInterval(gameplay, 1500);
+function gameplay() {
+    const randomHoleIndex = Math.floor(Math.random() * holes.length);
+    // Reset to all holes
+    for (let i = 0; i < holes.length; i++) {
+        holes[i].className = "hole";
+    }
+    holes[randomHoleIndex].classList.toggle('mole');
+}
