c247ef30f234edbcd61672ae1f9b947f5a555774
mbudak <promaxcoq@gmail.com>
2022-05-24 21:45:44 -0400

    ui update

--- async-js/index.js
+++ async-js/index.js
@@ -5,7 +5,7 @@ fetch('https://jsonplaceholder.typicode.com/users')
     .then((data) => {
         data.forEach(user => {
             userlist.innerHTML += `<div class="user-line" id="user_${user.id}">uid: [${user.id}] ${user.name} | ${user.email}</div>` + 
-            `<div class="post" id="userposts_${user.id}"><!-- placeholder --></div>`
+            `<div class="user-post" id="userposts_${user.id}"><!-- placeholder --></div>`
             fetchPostsByUserId(user.id)
         });
         
@@ -18,9 +18,12 @@ fetch('https://jsonplaceholder.typicode.com/posts?userId=' + uid)
         let user_posts = document.getElementById("userposts_" + uid);
         pdata.forEach(post => {
             
-            user_posts.innerHTML += `<div>
-                <div class="post-title">:: ${post.title}</div>
+            user_posts.innerHTML += `<div class="post">
+                <div class="post-title">
+                ${post.title}
                 <pre>${post.body}</pre>
+                </div>
+                
                 </div>`
             
         });
