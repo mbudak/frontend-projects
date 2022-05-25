1cd2169de10610b3594cd578bdd78d0712434184
mbudak <promaxcoq@gmail.com>
2022-05-24 21:23:43 -0400

    posts

---
+++ async-js/index.js
@@ -0,0 +1,34 @@
+var userlist = document.getElementById('userlist');
+
+
+
+
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(result => result.json())
+    .then((data) => {
+        data.forEach(user => {
+            userlist.innerHTML += `<div class="user-line" id="user_${user.id}">${user.id} : ${user.name} / ${user.email}</div>` + 
+            `<div class="post" id="userposts_${user.id}"><!-- placeholder --></div>`
+            fetchPostsByUserId(user.id)
+        });
+        
+}).catch(err => console.error(err));
+
+function fetchPostsByUserId(uid) {
+fetch('https://jsonplaceholder.typicode.com/posts?userId=' + uid)
+    .then(result => result.json())
+    .then((pdata) => {
+        let user_posts = document.getElementById("userposts_" + uid);
+        pdata.forEach(post => {
+            
+            user_posts.innerHTML += `<div>
+                <h4>${post.title}</h4>
+                <pre>${post.body}</pre>
+                </div>`
+            
+        });
+        
+}).catch(err => console.error(err));
+}
+
+
